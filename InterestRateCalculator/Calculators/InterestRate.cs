a74c9a9110055b07449ef0955bf00ad39d5325b9
Taner Cura <taner_cura@hotmail.com>
2019-03-31 14:19:57 +0100

    Add project files.

---
+++ InterestRateCalculator/Calculators/InterestRate.cs
@@ -0,0 +1,41 @@
+﻿using InterestRateCalculator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterestRateCalculator.Calculators
+{
+    public class InterestRate : IInterestRate
+    {
+        public void Run()
+        {
+            Console.WriteLine("Provide a balance:");
+            var input = Console.ReadLine();
+            double result = Calculate(double.Parse(input));
+            Console.WriteLine($"Your interest total will amount to: {result}");
+        }
+
+        public double Calculate(double balance)
+        {
+            if (balance > 0 && balance < 1000)
+                return balance * 0.01;
+
+            if (balance >= 1000 && balance < 5000)
+                return balance * 0.015;
+
+            if (balance >= 5000 && balance < 10000)
+                return balance * 0.02;
+
+            if (balance >= 10000 && balance < 50000)
+                return balance * 0.025;
+
+            if (balance > 50000)
+                return balance * 0.03;
+
+            // If no brackets are met then it must be less than or equal to zero
+            return 0;
+        }
+    }
+}
---
+++ InterestRateCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using InterestRateCalculator.Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterestRateCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Financial Calculator");
+            Console.WriteLine("Please select a calculator:");
+            Console.WriteLine("1. Calculate Interest Rates");
+            var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    InterestRate interestRate = new InterestRate();
+                    interestRate.Run();
+                    break;
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+
+        }
+    }
+}
