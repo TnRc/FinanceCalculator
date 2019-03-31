60aec8a343644ae33d2d51184219a5cef985ea46
Taner Cura <taner_cura@hotmail.com>
2019-04-01 00:25:25 +0100

    Refactoring

--- FinanceCalculator.Tests/InterestRateTests.cs
+++ FinanceCalculator.Tests/InterestRateTests.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FinanceCalculator.Calculators;
 using NUnit.Framework;
 
-namespace FinanceCalculator.Tests2
+namespace FinanceCalculator.Tests
 {
     [TestFixture]
     public class InterestRateTests
--- FinanceCalculator/Calculators/InterestRate.cs
+++ FinanceCalculator/Calculators/InterestRate.cs
@@ -1,11 +1,11 @@
-﻿using InterestRateCalculator.Interfaces;
+﻿using FinanceCalculator.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InterestRateCalculator.Calculators
+namespace FinanceCalculator.Calculators
 {
     public class InterestRate : IInterestRate
     {
